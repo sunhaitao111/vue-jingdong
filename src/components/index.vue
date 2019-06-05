08457a5883064e90c57899eaf5dfb0029c4f6c99
sunhaitao111 <973316286@qq.com>
2019-06-05 10:16:36 +0800

    jd

---
+++ src/components/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="index">
+    <div class="nav">
+      <div class="item" @click="toPath('/')" :class="this.$route.path === '/' ?  'active' : ''">
+        <i class="el-icon-s-home"></i>
+        <span>首页</span>
+      </div>
+      <div class="item" @click="toPath('/goodlist')" :class="this.$route.path === '/goodlist' ?  'active' : ''">
+        <i class="el-icon-s-fold"></i>
+        <span>分类</span>
+      </div>
+      <div class="item" @click="toPath('/car')" :class="this.$route.path === '/car' ?  'active' : ''">
+        <i class="el-icon-shopping-cart-full"></i>
+        <span>购物车</span>
+      </div>
+      <div class="item" @click="toPath('/user')" :class="this.$route.path === '/user' ?  'active' : ''">
+        <i class="el-icon-user"></i>
+        <span>我的</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  methods:{
+    toPath(path){
+      this.$router.replace(path)
+      console.log(this.$router);
+      console.log(this.$route)
+    }
+  }
+}
+</script>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  }
+  .index{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: 15px 0;
+    background: floralwhite;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.2);
+  }
+  .nav{
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    margin: 0 auto;
+  }
+  .item{
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+  }
+  .item:hover{
+    cursor: pointer;
+  }
+  .item.active{
+    color: red;
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import goodlist from './views/goodlist'
+import car from './views/car'
+import user from './views/user'
 Vue.use(Router)
 
 export default new Router({
@@ -14,12 +16,16 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path:'/goodlist'
+      ,component:goodlist
+    },
+    {
+      path:'/car'
+      , component:car
+    },
+    {
+      path:'/user',
+      component:user
     }
   ]
 })
